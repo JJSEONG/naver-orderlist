8df933383487fab2a0c00743bdd1af83061302a3
JJSEONG <hans3238@naver.com>
2022-05-04 22:52:55 +0900

    mac os db.json 연결

--- js/api/index.js
+++ js/api/index.js
@@ -1,5 +1,5 @@
 async function fetchGetRecentOrders() {
-  const response = await fetch('https://localhost:3000/recent_orders')
+  const response = await fetch('http://localhost:3000/recent-orders')
 
   const data = await response.json()
 

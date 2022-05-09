bff3d1fb3365f40aabcc63707347b07748826cdb
JJSEONG <hans3238@naver.com>
2022-05-09 23:48:18 +0900

    tablist components 분리

--- js/components/index.js
+++ js/components/index.js
@@ -1,5 +1,9 @@
-import Header from './Header.js'
-import MenuList from './MenuList.js'
+import Header from './Header.js';
+import MenuList from './MenuList.js';
+import OrderTypeList from './OrderTypeList.js';
+import TabList from './TabList.js';
 
 customElements.define('order-header', Header);
 customElements.define('menu-list', MenuList);
+customElements.define('order-type-list', OrderTypeList);
+customElements.define('tab-list', TabList);

053512a9e58077b6d3fbff56ebe073b8d129525d
JJSEONG <hans3238@naver.com>
2022-05-10 23:23:01 +0900

    Recent-menu-list components 분리

--- js/components/index.js
+++ js/components/index.js
@@ -2,8 +2,10 @@ import Header from './Header.js';
 import MenuList from './MenuList.js';
 import OrderTypeList from './OrderTypeList.js';
 import TabList from './TabList.js';
+import RecentMenuList from './RecentMenuList.js';
 
 customElements.define('order-header', Header);
 customElements.define('menu-list', MenuList);
 customElements.define('order-type-list', OrderTypeList);
 customElements.define('tab-list', TabList);
+customElements.define('recent-menu-list', RecentMenuList);
--- js/pages/MenuPage.js
+++ js/pages/MenuPage.js
@@ -85,8 +85,10 @@ export default class MenuPage extends View {
 
         <!-- 주문분류 -->
         <tab-list
-          .onChangeTab=${this.onChangeTab}
-        ></tab-list>
+          .tabIndex=${this.tabIndex}
+          .onChangeTab=${this.onChangeTab.bind(this)}
+        >
+        </tab-list>
 
         <div class="info-main-notice">
           ${ORDER_TYPE_MESSAGE[this.tabIndex]}
@@ -101,33 +103,10 @@ export default class MenuPage extends View {
         </div>
 
         <!-- 최근 주문 내역 -->
-        <div class="recent-order-area">
-          <div class="recent-title">
-            <img src="../assets/images/ico-clock.svg" alt="" class="ico-clock">최근<br>주문
-          </div>
-          <div class="recent-menu-area scroll-x">
-            <ul class="recent-menu-list">
-              ${this.recentMenuItems.map(
-                ({id, name, price, isPopular, imageUrl}) => html`
-              <li class="recent-menu-item is-ordered"
-                @click=${() => this.redirectDetailPage(id)}
-              >
-                <a>
-                  <div class="menu-img-area">
-                    ${isPopular ? html`<span class="badge-popular">인기</span>` : ''}
-                    <img class="menu-img" src="${imageUrl}" alt="메뉴사진">
-                  </div>
-                  <p class="menu-name">${name}</p>
-                  <p class="menu-price">${price}</p>
-                </a>
-                <a href="#" class="badge-cart">
-                  <img src="../assets/images/ico-cart.svg" alt="주문하기" class="ico-cart">
-                </a>
-              </li>`
-              )}
-            </ul>
-          </div>
-        </div>
+        <recent-menu-list
+          .recentMenuItems=${this.recentMenuItems}
+          .redirectDetailPage=${this.redirectDetailPage.bind(this)}
+        ></recent-menu-list>
         <!-- // 최근 주문 내역 -->
       </div>
     </div>

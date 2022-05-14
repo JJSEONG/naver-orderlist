af0dcfe9bc4b43c6c56ca355fa2e05d0e667fc23
JJSEONG <hans3238@naver.com>
2022-05-14 23:34:33 +0900

    장바구니 구현

--- js/pages/DetailPage.js
+++ js/pages/DetailPage.js
@@ -25,6 +25,7 @@ export default class DetailPage extends View {
     this.menuAmount = 1;
     this.orderTypeIndex = orderTypeIndex;
     this.orderType = orderTypeIndex === 0 ? '포장' : '매장';
+    this.isPopupOpen = false;
 
     const [menuID] = location.pathname.split('/').splice(-1);
 
@@ -45,7 +46,18 @@ export default class DetailPage extends View {
       orderType: {
         type: String,
       },
+      isPopupOpen: {
+        type: Boolean,
+      },
     };
+  }
+
+  openOrderPopup() {
+    this.isPopupOpen = true;
+  }
+
+  closeOrderPopup() {
+    this.isPopupOpen = false;
   }
 
   onIncreaseAmount() {
@@ -134,7 +146,7 @@ export default class DetailPage extends View {
                   onDecrease: this.onDecreaseAmount,
                 })}
               </div>
-              <button class="btn-order">
+              <button class="btn-order @click=${this.openOrderPopup}">
                 ${this.menuAmount}개 담기
                 ${this.menu.price * this.menuAmount}원
               </button>
@@ -144,6 +156,169 @@ export default class DetailPage extends View {
         </div>
         <!-- // 메뉴 주문 정보 영역 -->
       </div>
+
+      <!-- 옵션 팝업영역 -->
+      <div class="option-popup-area ${this.isPopupOpen ? 'show' : ''}">
+        <div class="dimmed-layer light"></div>
+        <div class="menu-option-popup">
+          <svg class="content-top-pattern" width="100%" height="100%">
+            <defs>
+                <pattern id="pattern-triangle" x="0" y="0" width="10" height="11" patternUnits="userSpaceOnUse">
+                    <polygon points="5 5, 10 10, 10 11, 0 11, 0 10"></polygon>
+                </pattern>
+            </defs>
+            <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern-triangle)"></rect>
+          </svg>
+
+          <div class="content-top">
+            <div class="common-inner">
+              <div class="menu-img-area">
+                <img src="${this.menu.imageUrl}" alt="${this.menu.name}" class="menu-img">
+              </div>
+              <div class="menu-detail-area">
+                <p class="menu-name">
+                  <span class="name">${this.menu.name}</span>
+                  <span class="badge">${this.menu.orderType}</span>
+                </p>
+                ${SpinButton({
+                  count: this.menuAmount,
+                  onIncrease: this.onIncreaseAmount,
+                  onDecrease: this.onDecreaseAmount,
+                })}
+              </div>
+              <button class="btn-close" @click=${this.closeOrderPopup}>
+                <img
+                  src="/assets/images/ico-close.svg"
+                  alt="order popup close button"
+                  class="ico-close"
+                />
+              </button>
+            </div>
+          </div>
+
+          <div class="content-body">
+            <div class="option-group">
+              <div class="option-title">
+                <p class="title">
+                  <span class="badge required">필수</span>
+                  <span class="text">베이스 선택</span>
+                </p>
+              </div>
+              <ul class="option-list">
+                <li class="option-item">
+                  <input type="radio" id="rd1" class="input-radio" name="base" checked>
+                  <label for="rd1" class="label">
+                    <span class="label-txt">추천대로</span>
+                    <span class="label-icon"></span>
+                  </label>
+                </li>
+                <li class="option-item">
+                  <input type="radio" id="rd2" class="input-radio" name="base">
+                  <label for="rd2" class="label">
+                    <span class="label-txt">채소볼</span>
+                    <span class="label-icon"></span>
+                  </label>
+                </li>
+                <li class="option-item">
+                  <input type="radio" id="rd3" class="input-radio" name="base">
+                  <label for="rd3" class="label">
+                    <span class="label-txt">곡물볼</span>
+                    <span class="label-icon"></span>
+                  </label>
+                </li>
+              </ul>
+            </div>
+
+            <div class="option-group">
+              <div class="option-title">
+                <p class="title">
+                  <span class="badge required">선택</span>
+                  <span class="text">토핑추가</span>
+                </p>
+                <p class="desc">최대 5개까지 선택할 수 있습니다.</p>
+              </div>
+              <ul class="option-list">
+                <li class="option-item">
+                  <input type="checkbox" id="chk1" class="input-check" checked>
+                  <label for="chk1" class="label">
+                    <span class="label-txt">채소추가(기본 제공량의 30% 추가) <span class="price">+900원</span></span>
+                    <span class="label-icon"></span>
+                  </label>
+                </li>
+                <li class="option-item">
+                  <input type="checkbox" id="chk2" class="input-check">
+                  <label for="chk2" class="label">
+                    <span class="label-txt">곡물추가(기본 제공량의 50% 추가) <span class="price">+900원</span></span>
+                    <span class="label-icon"></span>
+                  </label>
+                </li>
+                <li class="option-item">
+                  <input type="checkbox" id="chk3" class="input-check">
+                  <label for="chk3" class="label">
+                    <span class="label-txt">시저 드레싱 추가 <span class="price">+900원</span></span>
+                    <span class="label-icon"></span>
+                  </label>
+                </li>
+              </ul>
+            </div>
+
+            <div class="option-group">
+              <div class="option-title">
+                <p class="title">
+                  <span class="badge">선택</span>
+                  <span class="text">토핑추가</span>
+                </p>
+                <P class="desc">최대 5개까지 선택할 수 있습니다.</P>
+              </div>
+              <ul class="option-list">
+                <li class="option-item">
+                  <label class="label checked">
+                    <span class="label-txt">치킨
+                      <span class="price">+1,500원</span>
+                    </span>
+                  </label>
+                  <div class="amount-select">
+                    <button class="btn-minus enabled" aria-label="빼기" disabled></button>
+                    <span class="amount">2</span>
+                    <button class="btn-plus enabled" aria-label="더하기"></button>
+                  </div>
+                </li>
+
+                <li class="option-item">
+                  <label class="label">
+                    <span class="label-txt">에그
+                      <span class="price">+900원</span>
+                    </span>
+                  </label>
+                  <div class="amount-select">
+                    <button class="btn-minus" aria-label="빼기" disabled></button>
+                    <span class="amount">1</span>
+                    <button class="btn-plus enabled" aria-label="더하기"></button>
+                  </div>
+                </li>
+
+                <li class="option-item">
+                  <label class="label">
+                    <span class="label-txt">치킨소세지
+                      <span class="price">+1,900원</span>
+                    </span>
+                  </label>
+                  <div class="amount-select">
+                    <button class="btn-minus" aria-label="빼기" disabled></button>
+                    <span class="amount">1</span>
+                    <button class="btn-plus enabled" aria-label="더하기"></button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="content-bottom">
+            <button class="btn-order">1개 담기 9,999원</button>
+          </div>
+        </div>
+      </div>
+    <!-- // 옵션 팝업영역 -->
     </div>
     `
   }
